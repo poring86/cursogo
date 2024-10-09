2ba18b9e61f3b8f61d843a85d59e0754e80ce3ca
poring86 <matheuslino86a@gmail.com>
2024-10-09 15:33:16 -0400

    Structs 2

--- 11/main.go
+++ 11/main.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
+
 type Client struct {
 	Nome  string
 	Idade int
 	Ativo bool
+	Endereco
 }
 
 func main() {
@@ -16,5 +24,13 @@ func main() {
 	}
 	matheus.Ativo = false
 
+	matheus.Estado = "MS"
+	matheus.Endereco.Numero = 20
+
 	fmt.Println(matheus.Ativo)
+
+	fmt.Println(matheus.Endereco)
+	fmt.Println(matheus.Endereco.Estado)
+
+	fmt.Println(matheus.Numero)
 }
