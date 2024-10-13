b2ca14b8e698d2052f7ae7ae6e3537fcf9b295a0
poring86 <matheuslino86a@gmail.com>
2024-10-12 20:01:47 -0400

    conditions

---
+++ 20/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	a := 1
+	b := 2
+	c := 3
+
+	switch a {
+	case 1:
+		println("a")
+	case 2:
+		println("b")
+	case 3:
+		println("c")
+	}
+
+	if a > b && c > a {
+		println("a > b && c > a")
+	}
+}
