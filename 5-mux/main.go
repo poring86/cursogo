75890443aa224e91dfc81d01ed441fb6614cb873
poring86 <matheuslino86a@gmail.com>
2024-10-15 14:31:17 -0400

    Server mux

---
+++ 5-mux/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomeHandler)
+	mux.Handle("/blog", blog{title: "My Blog"}) // Corrigido para Handle
+
+	// Executa o servidor na porta 8080 em uma goroutine
+	go http.ListenAndServe(":8080", mux)
+
+	mux2 := http.NewServeMux()
+	mux2.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Matheus!"))
+	})
+
+	// Executa o segundo servidor na porta 8081
+	http.ListenAndServe(":8081", mux2)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+type blog struct {
+	title string
+}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(b.title))
+}
